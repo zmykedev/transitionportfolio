6bb3422793592835e7496b22af00de3b28b02435
zmykedev <zmaikol399@gmail.com>
2025-07-05 20:56:46 -0400

    deploy hotfix

--- src/components/Skills/Skils.tsx
+++ src/components/Skills/Skils.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef } from 'react';
 import { gsap } from 'gsap';
 import { useGSAP } from '@gsap/react';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
